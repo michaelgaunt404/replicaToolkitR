8544748ef7db4fd2672d959fde58bfd832d981db
mike gaunt <merkaberk91@yahoo.com>
2023-01-26 10:32:06 -0800

    last build
    last build for package - added data objects

--- code/script_run_replicaToolkitR.R
+++ code/script_run_replicaToolkitR.R
@@ -12,6 +12,9 @@ library(wellknown)
 location = "data/req_dev"
 folder = 'data_20230125_162034'
 
+#acquire data==================================================================
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 ##acquire data==================================================================
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -76,4 +79,76 @@ aggregate_network_links(
   ,folder = folder
   ,auto_save = T
 )
+
+#make package===================================================================
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+study_area_network = here(location, 'study_area_network') %>%
+  paste0(".shp") %>%  read_sf() %>%  st_transform(crs = 4326)
+usethis::use_data(study_area_network, overwrite = TRUE)
+
+poi_list = here(location, 'poi_list') %>%
+  paste0(".csv") %>%  read.csv()
+usethis::use_data(poi_list, overwrite = TRUE)
+
+
+acquired_sa_polys = here(location, folder, 'acquired_sa_polys') %>%
+  paste0(".gpkg") %>%  read_sf() %>%  st_transform(crs = 4326)
+usethis::use_data(acquired_sa_polys, overwrite = TRUE)
+
+aggregated_network_links = here(location, folder, 'aggregated_network_links') %>%
+  paste0(".rds")
+usethis::use_data(aggregated_network_links, overwrite = TRUE)
+
+replica_queried_network = here(location, folder, 'replica_queried_network') %>%
+  paste0(".csv") %>%  read.csv()
+usethis::use_data(replica_queried_network, overwrite = TRUE)
+
+replica_queried_network_cntds = here(location, folder, 'replica_queried_network_cntds') %>%
+  paste0(".gpkg") %>%  read_sf() %>%  st_transform(crs = 4326)
+usethis::use_data(replica_queried_network_cntds, overwrite = TRUE)
+
+replica_queried_network_links = here(location, folder, 'replica_queried_network_links') %>%
+  paste0(".gpkg") %>%  read_sf() %>%  st_transform(crs = 4326)
+usethis::use_data(replica_queried_network_links, overwrite = TRUE)
+
+replica_sa_poly_index = here(location, folder, 'replica_sa_poly_index') %>%
+  paste0(".csv") %>%  read.csv()
+usethis::use_data(replica_sa_poly_index, overwrite = TRUE)
+
+replica_trip_origin_destination = here(location, folder, 'replica_trip_origin_destination') %>%
+  paste0(".csv") %>%  read.csv()
+usethis::use_data(replica_trip_origin_destination, overwrite = TRUE)
+
+replica_trip_origin_links = here(location, folder, 'replica_trip_origin_links') %>%
+  paste0(".csv") %>%  read.csv()
+usethis::use_data(replica_trip_origin_links, overwrite = TRUE)
+
+replica_trip_origin_links_gpkg = here(location, folder, 'replica_trip_origin_links') %>%
+  paste0(".gpkg") %>%  read_sf() %>%  st_transform(crs = 4326)
+usethis::use_data(replica_trip_origin_links_gpkg, overwrite = TRUE)
+
+table_agg_by_link_subset_limited = here(location, folder, 'table_agg_by_link_subset_limited') %>%
+  paste0(".csv") %>%  read.csv()
+usethis::use_data(table_agg_by_link_subset_limited, overwrite = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
