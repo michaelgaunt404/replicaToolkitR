972bb8ee2a98170bcaa00c3d7b6d4ddc7f1d5a3a
mike gaunt <merkaberk91@yahoo.com>
2023-10-13 10:09:53 -0700

    add changes to view_replica_study_area_network
    add feature to prompt user about providing a link limit

--- R/view_replica_study_area_network.R
+++ R/view_replica_study_area_network.R
@@ -21,10 +21,43 @@
 #' view_replica_study_area_network("network_table", "customer_name")
 #' }
 view_replica_study_area_network = function(network_table
-                                           ,customer_name){
+                                           ,customer_name
+                                           ,links_pro = NA
+                                           ){
   message(str_glue("{gauntlet::strg_make_space_2()}Please draw a study area that will be used to query Replica's roadway network..."))
   message("Draw it as small and parsimonious as possible")
   message(str_glue("You can draw mulitple, discrete objects if you wish{gauntlet::strg_make_space_2(last = F)}"))
+
+  limit_links = robust_prompt_used("limit the number of links using the links supplied links_pro")
+
+  if (limit_links) {
+    if (is.na(links_pro)) {
+      message("User did not supply a links_pro input....")
+      continue_response <- robust_prompt_used("would you like to continue without limiting the number of links")
+      if (continue_response) {
+        links_response = c('highway','corridor','road'
+                           ,'motorway','motorway_link'
+                           ,'trunk','trunk_link'
+                           ,'primary','primary_link'
+                           ,'secondary','secondary_link'
+                           ,'tertiary','tertiary_link') %>%
+          paste0("'", ., "'", collapse = ", ")
+      } else {
+        stop("Execution terminated by user.")
+      }
+    } else {
+      links_response = links_pro
+    }
+  } else {
+    links_response = c('highway','corridor','road'
+                       ,'motorway','motorway_link'
+                       ,'trunk','trunk_link'
+                       ,'primary','primary_link'
+                       ,'secondary','secondary_link'
+                       ,'tertiary','tertiary_link') %>%
+      paste0("'", ., "'", collapse = ", ")
+  }
+
 
   study_area = mapedit::drawFeatures() %>% st_transform(4326)
   study_area_wkt = wellknown::sf_convert(st_union(study_area))
@@ -37,6 +70,7 @@ view_replica_study_area_network = function(network_table
                       ST_GEOGFROMTEXT('{study_area_wkt}')
                       ,geometry) as flag_contains
                       from `{network_table}`
+                      where highway in ({links_pro})
                       )
                       where flag_contains = TRUE"))
 
